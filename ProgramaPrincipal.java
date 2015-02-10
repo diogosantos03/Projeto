b9ab855734737d1e87418dac33a2d16a7b610f9d
Diogo Santos <diogo.santos@dce.ufpb..ufpb.br>
2015-02-09 22:47:44 -0300

    Main bem simples e o diagrama

---
+++ ProgramaPrincipal.java
@@ -0,0 +1,11 @@
+package br.com.ufpb.projetoDisciplinaPOO;
+
+public class ProgramaPrincipal {
+
+	public static void main(String[] args) {
+		GUI janela = new GUI();
+		janela.setVisible(true);
+
+	}
+
+}
